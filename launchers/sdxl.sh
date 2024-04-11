210236c5a582fcc36c8dc0f4f6bce81dd807d071
Yixin Fei <yixin.feiyx@gmail.com>
2024-04-11 14:42:40 -0400

    first commit

---
+++ launchers/sdxl.sh
@@ -0,0 +1,23 @@
+export MODEL_NAME="stabilityai/stable-diffusion-xl-base-1.0"
+export VAE="madebyollin/sdxl-vae-fp16-fix"
+export DATASET_NAME="GenAI"
+
+
+# Effective BS will be (N_GPU * train_batch_size * gradient_accumulation_steps)
+# Paper used 2048. Training takes ~30 hours / 200 steps
+
+CUDA_VISIBLE_DEVICES=0 accelerate launch train.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --pretrained_vae_model_name_or_path=$VAE \
+  --dataset_name=$DATASET_NAME \
+  --train_batch_size=1 \
+  --dataloader_num_workers=16 \
+  --gradient_accumulation_steps=128 \
+  --max_train_steps=2000 \
+  --lr_scheduler="constant_with_warmup" --lr_warmup_steps=200 \
+  --learning_rate=1e-8 --scale_lr \
+  --checkpointing_steps 200 \
+  --beta_dpo 5000 \
+   --sdxl  \
+  --output_dir="tmp-sdxl"
+  
